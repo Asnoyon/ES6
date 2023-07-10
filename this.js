cf1aa53581e86df38aa2bf0d102dd4adf25f5d90
Asnoyon <blackpain1100@gmail.com>
2023-07-10 10:23:27 +0600

    update index and this

--- this.js
+++ this.js
@@ -1,10 +1,21 @@
-var javascript = {
-    name: "Noyon",
-    libraries: ["React","Angular","Vue"],
-    printlibraries: function(){
-        this.libraries.forEach((a)=>{
-            console.log(`${this.name} loves ${a}`)
-        })
-    }
+// var javascript = {
+//     name: "Noyon",
+//     libraries: ["React","Angular","Vue"],
+//     printlibraries: function(){
+//         this.libraries.forEach((a)=>{
+//             console.log(`${this.name} loves ${a}`)
+//         })
+//     }
+// }
+// javascript.printlibraries();
+
+const searchInput = document.querySelector(".search");
+const display = document.querySelector(".result");
+const thanks = document.querySelector(".thanks");
+function show(){
+    display.innerHTML=this.value;
+    setTimeout(() => {
+        thanks.innerHTML= `You have typed: ${this.value}`
+    }, 1000);
 }
-javascript.printlibraries();
+searchInput.addEventListener("keyup", show)
