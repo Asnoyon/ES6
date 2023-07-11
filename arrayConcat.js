4341cf257ce15a1b51ae9368ffe062c2915900b6
Asnoyon <blackpain1100@gmail.com>
2023-07-11 15:52:11 +0600

    add arrayConcat

---
+++ arrayConcat.js
@@ -0,0 +1,15 @@
+// var numbers1 = [1,2,3,4,5];
+// var numbers2 = [6,7,8,9,10];
+// var result = numbers1.concat(numbers2);
+// console.log(result)
+// console.log(numbers1)
+
+
+//concat e array te kono change hoy na
+
+//array.push abar main array k change kore
+
+var numbers3 = [11,12,13,14];
+numbers3.push(15,16);
+
+console.log(numbers3);
