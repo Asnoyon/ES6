4f9172f538ca7083147ad894c4927fbebc8a02a6
Asnoyon <blackpain1100@gmail.com>
2023-07-26 12:07:09 +0600

    this update

--- this.js
+++ this.js
@@ -11,11 +11,11 @@
 
 const searchInput = document.querySelector(".search");
 const display = document.querySelector(".result");
-const thanks = document.querySelector(".thanks");
+const thank = document.querySelector(".thanks");
 function show(){
     display.innerHTML=this.value;
     setTimeout(() => {
-        thanks.innerHTML= `You have typed: ${this.value}`
+        thank.innerHTML= `You have typed: ${this.value}`
     }, 1000);
 }
 searchInput.addEventListener("keyup", show)
