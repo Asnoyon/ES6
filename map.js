740dbe3c2fba42e5ae28096552717389e9e82d0b
Asnoyon <blackpain1100@gmail.com>
2023-07-29 13:10:56 +0600

    map update

--- map.js
+++ map.js
@@ -1,8 +1,8 @@
-var number = [1, 2, 3, 4];
-var result = number.map((num) => {
+var numbers = [1, 2, 3, 4];
+var result = numbers.map((num) => {
   return num * 2;
 });
-console.log(number);
+console.log(numbers);
 console.log(result);
 
 //map new ekta array make kore and main array change kore na
