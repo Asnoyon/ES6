7515a5995f8e33d02434b37370642712fd79cfb1
Asnoyon <blackpain1100@gmail.com>
2023-08-03 20:40:16 +0600

    update

--- this.js
+++ this.js
@@ -12,10 +12,10 @@
 const searchInput = document.querySelector(".search");
 const display = document.querySelector(".result");
 const thanks = document.querySelector(".thanks");
-function show(){
-    display.innerHTML=this.value;
-    setTimeout(() => {
-        thanks.innerHTML= `You have typed: ${this.value}`
-    }, 1000);
+function show() {
+  display.innerHTML = this.value;
+  setTimeout(() => {
+    thanks.innerHTML = `You have typed: ${this.value}`;
+  }, 1000);
 }
-searchInput.addEventListener("keyup", show)
+searchInput.addEventListener("key up", show);
